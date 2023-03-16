1889611f847342f6c463c61b2cf2fb894b3abd81
satpreet07 <satpreetvirdi007@gmail.com>
2023-03-17 00:12:58 +0530

    First Commit

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,6 +1,21 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract RealEstate {
-
+contract RealEstate is ERC721URIStorage{
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+     constructor() ERC721("Real Estate", "Real") {}
+     function mint(string memory tokenURI)public returns(uint256){
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+     }
+     function totalSupply() public view returns (uint256){
+        return _tokenIds.current();
+     }
 }
